fc2df0031431f068db867436d3830c523336aca9
kawaii-flesh <47851113+kawaii-flesh@users.noreply.github.com>
2019-03-13 12:55:27 +0300

    Update boot.asm

--- Source/boot.asm
+++ Source/boot.asm
@@ -2,6 +2,22 @@
 ;Compiler: 	FASM(v 1.73.02 or better)
 ;License:	GPL 3.0
 
+;    KonsOS - The simple operating system is running in real mode.
+;    Copyright (C) 2019  kawaii-flesh
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+
+;    You should have received a copy of the GNU General Public License
+;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 use16
 org 0x7c00
 ;[==========[CONST]==========
